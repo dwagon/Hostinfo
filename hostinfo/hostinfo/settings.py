0b4dd20af7f434dc2e6c7b3e49bd894b457a54a3
Dougal Scott <dougal.scott@gmail.com>
2014-03-23 13:35:07 +1100

    Use psycopg2

--- hostinfo/hostinfo/settings.py
+++ hostinfo/hostinfo/settings.py
@@ -63,7 +63,7 @@ DATABASES = {
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'hostinfo',
         'USER': 'root',
         'PASSWORD': '',
