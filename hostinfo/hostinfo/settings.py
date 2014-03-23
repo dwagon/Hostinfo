42b6e463b033531676fb51d90948b033a072f264
Dougal Scott <dougal.scott@gmail.com>
2014-03-23 12:11:37 +1100

    Use Mysql

--- hostinfo/hostinfo/settings.py
+++ hostinfo/hostinfo/settings.py
@@ -58,9 +58,16 @@ WSGI_APPLICATION = 'hostinfo.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hostinfo',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
     }
 }
 
