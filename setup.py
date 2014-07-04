4ec58c51b77178e1d528049dd678b9ae827b1e0c
Dougal Scott <Dougal.Scott@gmail.com>
2014-07-04 21:57:41 +1000

    Include the reports in setup

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
-# 
+#
 # Setup script for hostinfo
 #
 # Written by Dougal Scott <dougal.scott@gmail.com>
 
 import os
-import sys
 from distutils.core import setup
 
-setup_scripts       = [os.path.join('bin', f) for f in  os.listdir('bin')]
-setup_templates     = {'hostinfo.host': ['templates/*']}
+setup_scripts = [os.path.join('bin', f) for f in os.listdir('bin')]
+setup_templates = {'hostinfo.host': ['templates/*'], 'hostinfo.report': ['templates/report/*']}
 
-setup_classifiers   = [
+setup_classifiers = [
     'Development Status :: 5 - Production/Stable',
     'Environment :: Console',
     'Environment :: Web Environment',
@@ -23,26 +22,27 @@ setup_classifiers   = [
     ]
 
 setup(
-    name            = 'hostinfo',
-    version         = '1.41',
-    description     = 'ITIL CMDB for systems administrators',
-    author          = 'Dougal Scott',
-    author_email    = 'dougal.scott@gmail.com',
-    url             = 'https://github.com.dwagon/Hostinfo',
-    requires        = ['Django (>=1.6)'],
-    scripts         = setup_scripts,
-    packages        = [
-        'hostinfo', 
-        'hostinfo.host', 
-        'hostinfo.host.commands', 
-        'hostinfo.host.autoupdaters', 
-        'hostinfo.host.links', 
-        'hostinfo.host.reports', 
-        'hostinfo.host.templatetags', 
-        'hostinfo.hostinfo', 
+    name='hostinfo',
+    version='1.41',
+    description='ITIL CMDB for systems administrators',
+    author='Dougal Scott',
+    author_email='dougal.scott@gmail.com',
+    url='https://github.com.dwagon/Hostinfo',
+    requires=['Django (>=1.6)'],
+    scripts=setup_scripts,
+    packages=[
+        'hostinfo',
+        'hostinfo.host',
+        'hostinfo.host.commands',
+        'hostinfo.host.autoupdaters',
+        'hostinfo.host.links',
+        'hostinfo.host',
+        'hostinfo.host.templatetags',
+        'hostinfo.hostinfo',
+        'hostinfo.report',
     ],
-    package_data    = setup_templates,
-    classifiers     = setup_classifiers,
+    package_data=setup_templates,
+    classifiers=setup_classifiers,
     )
 
 #EOF
