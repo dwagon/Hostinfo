b8738e34603aa2456885e7ae1ed8d20d4d94974a
Dougal Scott <Dougal.Scott@gmail.com>
2014-07-30 20:49:04 +1000

    Use the correct database

--- hostinfo/hostinfo/settings.py
+++ hostinfo/hostinfo/settings.py
@@ -64,7 +64,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'hostinfo',
-        'USER': 'root',
+        'USER': 'hostinfo',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
     }
