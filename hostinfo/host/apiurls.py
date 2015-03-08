8c0494b152e5586e694df8bbbb55e92cc06168bf
Dougal Scott <Dougal.Scott@gmail.com>
2015-03-08 16:58:21 +1100

    More work on alias rest interface

--- hostinfo/host/apiurls.py
+++ hostinfo/host/apiurls.py
@@ -2,14 +2,17 @@ from django.conf.urls import url, include
 
 import restviews
 
+hostspec = r'((?P<pk>[0-9]+?)|(?P<name>\S+?))'
+aliasspec = r'((?P<aliaspk>[0-9]+?)|(?P<alias>\S*?))'
+
 urlpatterns = [
     url(r'', include([
-        url(r'host/((?P<pk>[0-9]+)|(?P<name>\S+))/$', restviews.HostDetail, name='resthost'),
+        url(r'host/%s/alias/%s/?$' % (hostspec, aliasspec), restviews.HostAliasRest, name='restalias'),
+        url(r'host/%s/?$' % hostspec, restviews.HostDetail, name='resthost'),
         url(r'host/$', restviews.HostList),
-        url(r'key/((?P<pk>[0-9]+)|(?P<name>\S+))/$', restviews.KeyDetail, name='restkey'),
-        url(r'alias/((?P<pk>[0-9]+)|(?P<name>\S+))/$', restviews.AliasDetail, name='restalias'),
-        url(r'kval/(?P<pk>[0-9]+)/$', restviews.KValDetail, name='restkval'),
-        url(r'query/(?P<query>\S+)/$', restviews.HostQuery),
+        url(r'key/%s' % hostspec, restviews.KeyDetail, name='restkey'),
+        url(r'kval/(?P<pk>[0-9]+?)/$', restviews.KValDetail, name='restkval'),
+        url(r'query/(?P<query>\S+?)/$', restviews.HostQuery),
         ]
     ))
 ]
--- hostinfo/host/restviews.py
+++ hostinfo/host/restviews.py
@@ -1,16 +1,20 @@
 from .models import Host, AllowedKey, KeyValue, HostAlias, Links
-from .models import parseQualifiers, getMatches, getHost
+from .models import parseQualifiers, getMatches, getHost, HostinfoException
 from django.http import JsonResponse, Http404
 from django.shortcuts import get_object_or_404, get_list_or_404
 from django.core.urlresolvers import reverse
 from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
 
 
 ###############################################################################
 @require_http_methods(["GET"])
 def HostQuery(request, query):
     criteria = query.split('/')
-    qualifiers = parseQualifiers(criteria)
+    try:
+        qualifiers = parseQualifiers(criteria)
+    except HostinfoException as exc:
+        return JsonResponse({'error': str(exc)}, status=406)
     matches = getMatches(qualifiers)
     hosts = [Host.objects.get(id=pk) for pk in matches]
     ans = [HostShortSerialize(h, request) for h in hosts]
@@ -20,13 +24,53 @@ def HostQuery(request, query):
 ###############################################################################
 @require_http_methods(["GET"])
 def HostDetail(request, pk=None, name=None):
+    hostid = getReferredHost(pk, name)
+    return JsonResponse(HostSerialize(hostid, request))
+
+
+###############################################################################
+def getReferredHost(pk=None, name=None):
     if pk:
         hostid = get_object_or_404(Host, id=pk)
     elif name:
         hostid = getHost(hostname=name)
         if not hostid:
             raise Http404("Host %s does not exist" % name)
-    return JsonResponse(HostSerialize(hostid, request))
+    return hostid
+
+
+###############################################################################
+@require_http_methods(["GET", "POST", "DELETE"])
+@csrf_exempt
+def HostAliasRest(request, pk=None, name=None, aliaspk=None, alias=None):
+    status = 'ok'
+    hostid = getReferredHost(pk, name)
+    if request.method == "GET":
+        if not alias:
+            ha = get_list_or_404(HostAlias, hostid=hostid)
+        else:
+            if aliaspk:
+                ha = get_list_or_404(HostAlias, hostid=hostid, pk=aliaspk)
+            else:
+                ha = get_list_or_404(HostAlias, hostid=hostid, alias=alias)
+        sha = [HostAliasSerialize(h, request) for h in ha]
+        return JsonResponse({'status': status, 'aliases': sha})
+    elif request.method == "POST":
+        if HostAlias.objects.filter(hostid=hostid, alias=alias):
+            status = 'already exists'
+        else:
+            ha = HostAlias(hostid=hostid, alias=alias)
+            ha.save()
+            status = 'created'
+    elif request.method == "DELETE":
+        ha = get_object_or_404(HostAlias, hostid=hostid, alias=alias)
+        ha.delete()
+        status = 'deleted'
+
+    aliases = []
+    for h in HostAlias.objects.filter(hostid=hostid):
+        aliases.append(HostAliasSerialize(h, request))
+    return JsonResponse({'status': status, 'aliases': aliases})
 
 
 ###############################################################################
@@ -45,16 +89,6 @@ def KeyDetail(request, pk=None, name=None):
     elif name:
         keyid = get_object_or_404(AllowedKey, key=name)
     return JsonResponse(AllowedKeySerialize(keyid, request))
-
-
-###############################################################################
-@require_http_methods(["GET"])
-def AliasDetail(request, pk=None, name=None):
-    if pk:
-        keyid = get_object_or_404(HostAlias, id=pk)
-    elif name:
-        keyid = get_object_or_404(HostAlias, alias=name)
-    return JsonResponse(HostAliasSerialize(keyid, request))
 
 
 ###############################################################################
@@ -153,7 +187,7 @@ def KeyValueSerialize(obj, request):
 def HostAliasSerialize(obj, request):
     ans = {
         'id': obj.id,
-        'url': request.build_absolute_uri(reverse('restalias', args=(obj.id,))),
+        'url': request.build_absolute_uri(reverse('restalias', args=(obj.hostid.hostname, obj.id,))),
         'host': HostShortSerialize(obj.hostid, request),
         'alias': obj.alias,
         'origin': obj.origin,
