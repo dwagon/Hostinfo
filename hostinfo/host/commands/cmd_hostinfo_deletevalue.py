aeed5ca36f665351f10e0cee033808238912ffd1
Dougal Scott <Dougal.Scott@gmail.com>
2016-09-21 08:51:39 +1000

    Updated requirements

--- hostinfo/host/commands/cmd_hostinfo_deletevalue.py
+++ hostinfo/host/commands/cmd_hostinfo_deletevalue.py
@@ -1,7 +1,7 @@
 #
 # Written by Dougal Scott <dougal.scott@gmail.com>
 #
-#    Copyright (C) 2012 Dougal Scott
+#    Copyright (C) 2016 Dougal Scott
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -65,4 +65,4 @@ class Command(HostinfoCommand):
                         raise HostinfoException("Cannot delete a readonly value")
         return None, 0
 
-#EOF
+# EOF
