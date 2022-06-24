8269ede49c4c4f045382d3f850a3888497b42ff4
Dougal Scott <dougal.scott@gmail.com>
2022-06-24 18:58:34 +1000

    Update pingCheck

--- contrib/auto_updaters/pingCheck.py
+++ contrib/auto_updaters/pingCheck.py
@@ -1,45 +1,91 @@
-#!/usr/bin/env python
-#
-# Script to ping every host and update the 'lastping' key
-# to give an indication of 'liveness'
-#
-# Writen by Dougal Scott <dwagon@pobox.com>
-# $Id: pingCheck.py 80 2011-02-15 11:13:29Z dougal.scott@gmail.com $
-# $HeadURL: https://hostinfo.googlecode.com/svn/trunk/contrib/auto_updaters/pingCheck.py $
+#!/usr/bin/env python3
+"""
+Script to ping every host and update the 'lastping' key
+to give an indication of 'liveness'
+"""
+# Writen by Dougal Scott <dougal.scott@gmail.com>
 
-import os, time
-
-############################################################################
-def pingCheck(hostname):
-    f = os.popen("/usr/sbin/ping %s 2>/dev/null" % hostname)
-    rc = f.close()
-    if not rc:
-        return True
-    return False
+import argparse
+import os
+import subprocess
+from multiprocessing import Pool
 
 
-################################################################################
-def getHostlist(hostargs=""):
+###############################################################################
+def get_host_list(hostargs=""):
+    """Get the list of hosts to check"""
     tmp = []
-    f = os.popen("hostinfo %s" % hostargs)
-    for line in f:
-        hostname = line.strip()
-        tmp.append(Host(hostname))
-    f.close()
+    cmd = f"hostinfo {hostargs}"
+    with os.popen(cmd) as hinfh:
+        for line in hinfh:
+            hostname = line.strip()
+            tmp.append(hostname)
     return tmp
 
 
-################################################################################
-def main(hostargs=""):
-    print("origin=pingCheck")
-    today = time.strftime("%Y-%m-%d")
-    hostlist = getHostlist(hostargs)
-    for host in hostlist:
-        if pingCheck(host):
-            print("%s lastping=%s" % (hostname, today))
+###############################################################################
+def parse_args():
+    """Parse command line arguments"""
+    parser = argparse.ArgumentParser(description="Ping everything")
+    parser.add_argument(
+        "--numthreads",
+        action="store",
+        type=int,
+        help="Number of threads to run in parallel",
+        default=None,
+    )
+    parser.add_argument(
+        "--hostargs",
+        action="store",
+        help="Args to hostinfo to limit hosts",
+        type=str,
+        default="",
+    )
+    args = parser.parse_args()
+    return args
 
 
-################################################################################
+###########################################################################
+def pingCheck(hostname: str) -> bool:
+    """Ping the hostname to see if it is up"""
+    retcode = subprocess.call("/bin/ping", "-c", "1", hostname)
+    if retcode < 0:
+        return False
+    return True
+
+
+###########################################################################
+def updateHost(hostname: str) -> None:
+    """Update hostinfo with the new ping date"""
+    subprocess.call(
+        "hostinfo_addvalue",
+        "--origin",
+        "pingCheck",
+        "--update",
+        "lastping=today",
+        hostname,
+    )
+
+
+###############################################################################
+def pinger(hostname):
+    """Check and update host if required"""
+    print(hostname)
+    if pingCheck(hostname):
+        updateHost(hostname)
+
+
+###############################################################################
+def main():
+    """mainline"""
+    args = parse_args()
+    host_list = get_host_list(args.hostargs)
+    print(host_list)
+    with Pool(args.numthreads) as pool:
+        pool.map(pinger, host_list)
+
+
+###############################################################################
 if __name__ == "__main__":
     main()
 
